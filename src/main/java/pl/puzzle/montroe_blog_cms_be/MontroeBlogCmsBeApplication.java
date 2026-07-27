7cdb2f5a533eafff7154b30918dd73301b223227
julia-kocik <julia.kocik@gmail.com>
2026-07-27 14:28:32 +0200

    Init commit

---
+++ src/main/java/pl/puzzle/montroe_blog_cms_be/MontroeBlogCmsBeApplication.java
@@ -0,0 +1,13 @@
+package pl.puzzle.montroe_blog_cms_be;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MontroeBlogCmsBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MontroeBlogCmsBeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pl/puzzle/montroe_blog_cms_be/MontroeBlogCmsBeApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.puzzle.montroe_blog_cms_be;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MontroeBlogCmsBeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
