79553514b1d933c01e870d36a98bdd41f176e6a0
julia-kocik <julia.kocik@gmail.com>
2026-08-12 16:06:52 +0200

    Add ArticleCreateService

--- src/main/java/pl/puzzle/montroe_blog_cms_be/article/ArticleCreateService.java
+++ src/main/java/pl/puzzle/montroe_blog_cms_be/article/ArticleCreateService.java
@@ -1,23 +1,55 @@
 package pl.puzzle.montroe_blog_cms_be.article;
 
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 import pl.puzzle.montroe_blog_cms_be.article.dto.ArticleCreateRequest;
-
-
+import pl.puzzle.montroe_blog_cms_be.article_section.ArticleSection;
+import pl.puzzle.montroe_blog_cms_be.article_summary_item.ArticleSummaryItem;
+import pl.puzzle.montroe_blog_cms_be.article_table_of_content_item.ArticleTableOfContentItem;
 
 @Service
 public class ArticleCreateService {
+
     private final ArticleRepository articleRepository;
 
     public ArticleCreateService(ArticleRepository articleRepository) {
         this.articleRepository = articleRepository;
     }
 
-    public Article createArticle(ArticleCreateRequest articleCreateRequest) {
+    @Transactional
+    public Article createArticle(ArticleCreateRequest request) {
+        Article article = Article.createArticle(request);
 
-        Article article = Article.createArticle(articleCreateRequest);
-        articleRepository.save(article);
+        for (int i = 0; i < request.summaryItems().size(); i++) {
+            ArticleSummaryItem item = ArticleSummaryItem.create(
+                    request.summaryItems().get(i),
+                    article,
+                    i + 1
+            );
 
-        return article;
+            article.addSummaryItem(item);
+        }
+
+        for (int i = 0; i < request.sections().size(); i++) {
+            ArticleSection section = ArticleSection.create(
+                    request.sections().get(i),
+                    article,
+                    i + 1
+            );
+
+            article.addSection(section);
+        }
+
+        for (int i = 0; i < request.tableOfContentItems().size(); i++) {
+            ArticleTableOfContentItem item = ArticleTableOfContentItem.create(
+                    request.tableOfContentItems().get(i),
+                    article,
+                    i + 1
+            );
+
+            article.addTableOfContentItem(item);
+        }
+
+        return articleRepository.save(article);
     }
 }
--- src/main/java/pl/puzzle/montroe_blog_cms_be/article/dto/ArticleCreateRequest.java
+++ src/main/java/pl/puzzle/montroe_blog_cms_be/article/dto/ArticleCreateRequest.java
@@ -1,4 +1,30 @@
 package pl.puzzle.montroe_blog_cms_be.article.dto;
 
-public record ArticleCreateRequest(String name, String lead) {
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotEmpty;
+import pl.puzzle.montroe_blog_cms_be.article_section.dto.ArticleSectionCreateRequest;
+import pl.puzzle.montroe_blog_cms_be.article_summary_item.dto.ArticleSummaryItemCreateRequest;
+import pl.puzzle.montroe_blog_cms_be.article_table_of_content_item.dto.ArticleTableOfContentItemCreateRequest;
+
+import java.util.List;
+
+public record ArticleCreateRequest(
+
+        @NotBlank
+        String name,
+
+        @NotBlank
+        String lead,
+
+        @NotEmpty
+        List<@Valid ArticleSummaryItemCreateRequest> summaryItems,
+
+        @NotEmpty
+        List<@Valid ArticleSectionCreateRequest> sections,
+
+        @NotEmpty
+        List<@Valid ArticleTableOfContentItemCreateRequest> tableOfContentItems
+
+) {
 }
